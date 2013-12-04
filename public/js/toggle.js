61be4d2202f8f6c9174ce94bc9378b95a700f4ee
Matt Campbell <mecampbell25@gmail.com>
2013-12-03 19:04:59 -0500

    small view changes

--- public/js/toggle.js
+++ public/js/toggle.js
@@ -1,8 +1,13 @@
 $('.show_readme').show(); //show buttons
 $('.readme').hide(); // hide all readmes
-
+ 
 $('.show_readme').click(function(){
     var readme_id = $(this).data('id');
-    $('.readme').hide();
-    $('#p'+readme_id).fadeTo(200, 1);
+    if ($('#p'+readme_id).css('display')!='none'){
+      $('.readme').hide();
+    } 
+    else{
+      $('.readme').hide();
+      $('#p'+readme_id).fadeToggle(200, "linear");
+    }  
 });
