3c87e191bbe13ecca5644566bcf779a6fb6e3181
Matt Campbell <mecampbell25@gmail.com>
2013-12-07 01:11:54 -0500

    octokit working

--- config/initializers/octokit.rb
+++ config/initializers/octokit.rb
@@ -1,13 +1,50 @@
+require 'octokit'
+require 'pry'
+require 'yaml'
+require 'dotenv'
+
+Dotenv.load
+
+binding.pry
+    
 # TODO: use this to check all flatiron repos for readme; parse the readme for yaml tags; do the usual magic
 
 # Provide authentication credentials
-client = Octokit::Client.new :access_token => ENV['GITHUB_OAUTH_TOKEN']
+client = Octokit::Client.new \
+  :client_id     => ENV["GITHUB_CLIENT_ID"],
+  :client_secret => ENV["GITHUB_CLIENT_SECRET"]
 
-FlatironRepos = client.repositories('flatiron-school')
-# full_names = FlatironRepos.map do |repo|
-#   repo.full_name if Octokit.readme(repo)
-# end
+# client = Octokit::Client.new :login => 'mecampbellsoup', :password => 'imahoe!1'
 
-# readmes = full_names.each do |name|
-#   Octokit.readme name, :accept => 'application/vnd.github.html'
-# end
+# Getting all the repos associated with the argument/account passed to the method
+TestRepos = client.repositories('mecampbellsoup')
+
+# Getting an array of the full names of each repo
+full_names = TestRepos.map do |repo|
+  repo.full_name
+end
+
+# Getting an array of the repos, as hashes, which have READMEs available (i.e. respond to the .readme method)
+readmes = full_names.map do |name|
+  begin
+  link = (client.readme name)._links.html
+  content = client.readme name, :accept => 'application/vnd.github.VERSION.raw'
+  rescue
+    next
+  end
+  { :link => link, :content => content }
+end.compact.reject { |hash| hash[:content] == nil || hash[:link] == nil }
+
+# Getting an array of the repos which have READMEs that respond to YAML.load
+yaml_from_readmes = readmes.map do |lab_hash|
+  begin
+    yaml_tags = YAML.load(lab_hash[:content])
+  rescue
+    next
+  end
+  { :link => lab_hash[:link], :yaml_tags => yaml_tags }
+end.compact.select { |yaml_readme| yaml_readme.is_a? Hash }
+
+# Now I have an array of links and the associated YAML tags...
+
+binding.pry
